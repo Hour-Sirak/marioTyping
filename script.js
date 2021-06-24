604fba56e8541e58cff57f51c50acf3c6f6bbdef
Hour-Sirak <hoursirak0102@gmail.com>
2021-06-24 19:09:59 +0700

    1black-screen

---
+++ script.js
@@ -0,0 +1,27 @@
+let btns = document.querySelectorAll('.btn-group .btn')
+let container = document.getElementById('container')
+let [blackScreenTop, blackScreenBottom] = document.querySelectorAll('.black-screen')
+let blackScreen = container.firstElementChild
+
+function reset(){
+	btns[0].classList.add('hover')
+	blackScreenTop.classList.remove('black-screen-top')
+	blackScreenBottom.classList.remove('black-screen-bottom')
+
+}
+
+function main(){
+	
+	btns[0].onclick = function(){
+		// blackScreen.style.transform = 'translateY(0%)';
+		// blackScreenTop.classList.add('black-screen-top')
+		// blackScreenBottom.classList.add('black-screen-bottom')
+		blackScreen.classList.add('black-screen')
+		blackScreen.style.backgroundColor = 'rgba(0, 0, 0, 1)'
+		
+		btns[0].classList.remove('hover')
+		setTimeout(reset, 3500)
+	}
+}
+
+window.onload = main
